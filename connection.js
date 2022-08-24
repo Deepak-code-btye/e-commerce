818cd2c942b359c2c63f87d631c89f79913e7db2
Deepak kumar <dk815617@gmail.com>
2022-08-24 17:53:48 +0530

    ecommerce backend

--- connection.js
+++ connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-const connectionStr = "Your mongoose connection string";
+const connectionStr = "mongodb://localhost:27017/kundali";
 
-mongoose.connect(connectionStr, {useNewUrlparser: true})
-.then(() => console.log('connected to mongodb'))
-.catch(err => console.log(err))
+mongoose.connect(connectionStr, { useNewUrlparser: true })
+  .then(() => console.log('connected to mongodb'))
+  .catch(err => console.log(err))
 
 mongoose.connection.on('error', err => {
   console.log(err)
